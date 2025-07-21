bc5f55de04327c942d64652b88db637a7d4f2ab3
Dmytro Nikolaenko <nikodim7474@proton.me>
2025-07-21 18:20:11 +0300

    new lesson homework add

---
+++ lesson12/homework.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on: 21.07.25 18:14
+@author: Dmytro Nikolaenko (nikodim74@gmail.com)
+@user: d.nikolaenko
+@project name: Python3_courses_homework
+@file: homework.py
+"""
